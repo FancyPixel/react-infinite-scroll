3dbbdf089d3f67e8f7ebf9ece1ed7ac78c095860
Andrea Mazzini <andrea.mazzini@gmail.com>
2016-01-20 11:55:01 +0100

    Fix infinite loop. Add react 0.14 compat

--- src/react-infinite-scroll.js
+++ src/react-infinite-scroll.js
@@ -1,3 +1,5 @@
+var deepEqual = require('deep-equal');
+
 function topPosition(domElt) {
   if (!domElt) {
     return 0;
@@ -5,41 +7,59 @@ function topPosition(domElt) {
   return domElt.offsetTop + topPosition(domElt.offsetParent);
 }
 
-module.exports = function (React) {
+module.exports = function (React, ReactDOM) {
   if (React.addons && React.addons.InfiniteScroll) {
     return React.addons.InfiniteScroll;
   }
+
   React.addons = React.addons || {};
+
   var InfiniteScroll = React.addons.InfiniteScroll = React.createClass({
+    displayName: 'InfiniteScroll',
+
+    propTypes: {
+      pageStart: React.PropTypes.number,
+      threshold: React.PropTypes.number,
+      loadMore: React.PropTypes.func.isRequired,
+      hasMore: React.PropTypes.bool
+    },
+
     getDefaultProps: function () {
       return {
         pageStart: 0,
         hasMore: false,
-        loadMore: function () {},
         threshold: 250
       };
     },
+
     componentDidMount: function () {
-      this.pageLoaded = this.props.pageStart;
       this.attachScrollListener();
     },
+
+    shouldComponentUpdate: function(nextProps, nextState) {
+      return !deepEqual(this.props.children, nextProps.children);
+    },
+
     componentDidUpdate: function () {
       this.attachScrollListener();
     },
+
     render: function () {
       var props = this.props;
       return React.DOM.div(null, props.children, props.hasMore && (props.loader || InfiniteScroll._defaultLoader));
     },
+
     scrollListener: function () {
-      var el = this.getDOMNode();
+      var el = ReactDOM.findDOMNode(this);
       var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
       if (topPosition(el) + el.offsetHeight - scrollTop - window.innerHeight < Number(this.props.threshold)) {
         this.detachScrollListener();
         // call loadMore after detachScrollListener to allow
         // for non-async loadMore functions
-        this.props.loadMore(this.pageLoaded += 1);
+        this.props.loadMore(this.props.pageStart += 1);
       }
     },
+
     attachScrollListener: function () {
       if (!this.props.hasMore) {
         return;
@@ -48,16 +68,20 @@ module.exports = function (React) {
       window.addEventListener('resize', this.scrollListener);
       this.scrollListener();
     },
+
     detachScrollListener: function () {
       window.removeEventListener('scroll', this.scrollListener);
       window.removeEventListener('resize', this.scrollListener);
     },
+
     componentWillUnmount: function () {
       this.detachScrollListener();
     }
   });
+
   InfiniteScroll.setDefaultLoader = function (loader) {
     InfiniteScroll._defaultLoader = loader;
   };
+
   return InfiniteScroll;
 };
